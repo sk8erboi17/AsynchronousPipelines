8f20e2ead9cdfe0bca4f4cf5d8267d9dc578e093
Peppino's Stuff <125193665+PeppinoTechTrends@users.noreply.github.com>
2023-02-13 21:36:08 +0100

    Update ReadingDataListener.java

--- src/main/java/net/techtrends/server/events/input/ReadingDataListener.java
+++ src/main/java/net/techtrends/server/events/input/ReadingDataListener.java
@@ -17,25 +17,25 @@ public abstract class ReadingDataListener<T> {
     /**
      * Checks if the server socket channel is open.
      *
-     * @param serverSocketChannel The server socket channel to check if it is open.
+     * @param socketChannel The server socket channel to check if it is open.
      * @return True if the server socket channel is open, false otherwise.
      */
-    private boolean isServerOpen(AsynchronousSocketChannel serverSocketChannel) {
-        return serverSocketChannel != null ;
+    private boolean isServerOpen(AsynchronousSocketChannel socketChannel) {
+        return socketChannel != null ;
     }
 
 
     /**
      * Initiates a read operation on the specified asynchronous server socket channel.
      *
-     * @param asyncServerSocket The asynchronous server socket channel to read data from.
+     * @param socketChannel The asynchronous socket channel to read data from.
      * @param callback          The response callback to be executed once data is read from the server socket channel.
      */
-    public void readData(AsynchronousSocketChannel asyncServerSocket, ResponseCallback<T> callback) {
-        if (isServerOpen(asyncServerSocket)) {
+    public void readData(AsynchronousSocketChannel socketChannel, ResponseCallback<T> callback) {
+        if (isServerOpen(socketChannel)) {
             if (!isReading) {
                 setReading(true);
-                readNextData(asyncServerSocket,callback);
+                readNextData(socketChannel,callback);
             }
         }
     }
@@ -48,7 +48,7 @@ public abstract class ReadingDataListener<T> {
      * An abstract method that should be implemented by concrete implementations of this class to initiate a read operation
      * on the specified asynchronous server socket channel.
      *
-     * @param asyncServerSocket The asynchronous server socket channel to read data from.
+     * @param socketChannel The asynchronous socket channel to read data from.
      */
-    protected abstract void readNextData(AsynchronousSocketChannel asyncServerSocket, ResponseCallback<T> callback);
+    protected abstract void readNextData(AsynchronousSocketChannel socketChannel, ResponseCallback<T> callback);
 }
