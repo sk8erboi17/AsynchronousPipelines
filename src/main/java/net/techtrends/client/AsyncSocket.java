d9e02129c5cd3c0c37fba33889442c0a496a8036
Peppino's Stuff <125193665+PeppinoTechTrends@users.noreply.github.com>
2023-04-26 23:47:34 +0200

    Add Close Method to AsyncSocket Class

--- src/main/java/net/techtrends/client/AsyncSocket.java
+++ src/main/java/net/techtrends/client/AsyncSocket.java
@@ -6,25 +6,26 @@ import java.nio.channels.AsynchronousSocketChannel;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+
 /**
- * AsyncSocket is class that provides a static method for creating a client connection using an AsynchronousSocketChannel.
+ * AsyncSocket is class that provides a static method for creating and close a client connection using an AsynchronousSocketChannel.
  */
 public class AsyncSocket {
 
-    /**
-     * Creates a client connection using the specified InetSocketAddress and returns an AsynchronousSocketChannel.
-     *
-     * @param inetSocketAddress the address and port of the server to connect to
-     * @return an AsynchronousSocketChannel that represents the connected client
-     * @throws IOException if an I/O error occurs
-     * @throws ExecutionException if the operation cannot be completed
-     * @throws InterruptedException if the current thread was interrupted while waiting
-     */
     public static AsynchronousSocketChannel createClient(InetSocketAddress inetSocketAddress) throws IOException, ExecutionException, InterruptedException {
         AsynchronousSocketChannel socketChannel = AsynchronousSocketChannel.open();
         Future<Void> connectFuture = socketChannel.connect(inetSocketAddress);
         connectFuture.get();
         return socketChannel;
     }
+
+    public static void closeSocketChannel(AsynchronousSocketChannel socketChannel) {
+        try {
+            socketChannel.close();
+        } catch (IOException e) {
+            System.err.println("Failed to close socket channel: " + e.getMessage());
+        }
+    }
+
 
 }
