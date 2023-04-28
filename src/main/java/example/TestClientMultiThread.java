8b752b1b6c532215372f7a782606f485186e0daf
Peppino's Stuff <125193665+PeppinoTechTrends@users.noreply.github.com>
2023-04-28 21:54:14 +0200

    Update TestClientMultiThread.java

--- src/main/java/example/TestClientMultiThread.java
+++ src/main/java/example/TestClientMultiThread.java
@@ -26,8 +26,8 @@ public class TestClientMultiThread {
     public static void main(String[] args) {
         try {
             AsynchronousSocketChannel socketChannel = AsyncSocket.createClient(new InetSocketAddress("localhost", 8080));
-            OutputListener outputListener1 = new OutputListener(socketChannel, 4,true);
-            OutputListener outputListener2 = new OutputListener(socketChannel, 4,true);
+            OutputListener outputListener1 = new OutputListener(socketChannel, 32,true);
+            OutputListener outputListener2 = new OutputListener(socketChannel, 32,true);
 
             Runnable thread1 = () -> {
                 for (int i = 0; i < 600; i++) {
